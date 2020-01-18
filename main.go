a862acce1a83f6907f42e27e03cb8c1e9f492a61
С.В <sv@sv-it.ru>
2020-01-18 21:18:43 +0000

    add func connectdb

--- main.go
+++ main.go
@@ -13,6 +13,22 @@ import (
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+func ConnectDB(client *mongo.Client){
+    // Create connect
+    err := client.Connect(context.TODO())
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    // Check the connection
+    err = client.Ping(context.TODO(), nil)
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    fmt.Println("Connected to MongoDB!")
+}
+
 func UpdateStatistic(msg *tgbotapi.Message, client *mongo.Client){
     collectionUsers := client.Database("test").Collection("users")
     collectionChats := client.Database("test").Collection("chats")
@@ -49,59 +65,46 @@ func UpdateStatistic(msg *tgbotapi.Message, client *mongo.Client){
 
 func bot(){
     bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
-        if err != nil {
-                log.Panic(err)
-        }
+    if err != nil {
+      log.Panic(err)
+    }
 
-        bot.Debug = false
+    bot.Debug = false
 
-        log.Printf("Authorized on account %s", bot.Self.UserName)
+    log.Printf("Authorized on account %s", bot.Self.UserName)
 
-        u := tgbotapi.NewUpdate(0)
-        u.Timeout = 60
+    u := tgbotapi.NewUpdate(0)
+    u.Timeout = 60
 
-        updates, err := bot.GetUpdatesChan(u)
+    updates, err := bot.GetUpdatesChan(u)
 
-    // Create client
     client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
     if err != nil {
       log.Fatal(err)
     }
 
-    // Create connect
-    err = client.Connect(context.TODO())
-    if err != nil {
-      log.Fatal(err)
-    }
+    ConnectDB(client)
 
-    // Check the connection
-    err = client.Ping(context.TODO(), nil)
-    if err != nil {
-      log.Fatal(err)
-    }
+    for update := range updates {
 
-    fmt.Println("Connected to MongoDB!")
+      if update.Message == nil { // ignore any non-Message Updates
+        continue
+      }
 
-        for update := range updates {
+      if strings.Contains(update.Message.Text, "@KangBongSungBot") {
+        if update.Message.From.ID == 533587790 {
+          msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Моя Настенька! 🤗")
+          msg.ReplyToMessageID = update.Message.MessageID
 
-                if update.Message == nil { // ignore any non-Message Updates
-                        continue
-                }
-
-                if strings.Contains(update.Message.Text, "@KangBongSungBot") {
-		  if update.Message.From.ID == 533587790 {
-		    msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Моя Настенька! 🤗")
-		    msg.ReplyToMessageID = update.Message.MessageID
-
-		    bot.Send(msg)
-		  }
-		}
-
-		UpdateStatistic(update.Message, client)
-
-		fmt.Printf("%d --- %s --- %d: %s\n", update.Message.From.ID, update.Message.From.FirstName, update.Message.Chat.ID, update.Message.Text)
-
+          bot.Send(msg)
         }
+      }
+
+    UpdateStatistic(update.Message, client)
+
+    fmt.Printf("%d --- %s --- %d: %s\n", update.Message.From.ID, update.Message.From.FirstName, update.Message.Chat.ID, update.Message.Text)
+
+    }
 }
 
 func main() {
