85cec046585a3d90ff32ca2fef0f2dedadb47c91
С.В <sv@sv-it.ru>
2020-01-18 15:38:38 +0000

    send to mongo

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import (
 	"log"
 	"os"
 	"fmt"
+	"context"
 
+        "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
@@ -23,6 +27,27 @@ func bot(){
 
         updates, err := bot.GetUpdatesChan(u)
 
+    // Create client
+    client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    // Create connect
+    err = client.Connect(context.TODO())
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    // Check the connection
+    err = client.Ping(context.TODO(), nil)
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    fmt.Println("Connected to MongoDB!")
+    collection := client.Database("test").Collection("chats")
+
         for update := range updates {
 
                 if update.Message == nil { // ignore any non-Message Updates
@@ -32,6 +57,17 @@ func bot(){
                 msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
                 msg.ReplyToMessageID = update.Message.MessageID
                 fmt.Println(update.Message.Text)
+		user := update.Message.From
+		opts := options.Update().SetUpsert(true)
+		filter := bson.D{{"id", user.ID}}
+		newitem := bson.D{{"$set", user, }}
+		updateResult, err := collection.UpdateOne(context.TODO(), filter, newitem, opts)
+                if err != nil {
+                  log.Fatal(err)
+                }
+
+		fmt.Printf("Matched %v documents and updated %v documents.\n", updateResult.MatchedCount, updateResult.ModifiedCount)
+		fmt.Printf("%d --- %s\n", user.ID, user.FirstName)
 
                 if _, err := bot.Send(msg); err != nil {
                         log.Panic(err)
